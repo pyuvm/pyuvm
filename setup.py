793792be758e469b50bc93f972a9e3cd0f82b19f
Ray Salemi <ray@raysalemi.com>
2018-04-01 10:36:32 -0400

    Now have a standard directory.

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='pyuvm',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Python Implementation of Universal Verification Methdology',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Ray Salemi',
+    author_email='ray@raysalemi.com',
+    url='https://rayboston@bitbucket.org/rayboston/pyuvm.git',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
