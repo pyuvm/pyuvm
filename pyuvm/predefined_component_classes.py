cc1aea7a0a58daf444fc2fdcdc17fb4441e7776e
Ray Salemi <ray@raysalemi.com>
2018-04-07 11:42:14 -0400

    Implemented up to 13.9
    Now need to figure out subscribers.

--- pyuvm/predefined_component_classes.py
+++ pyuvm/predefined_component_classes.py
@@ -1,4 +1,5 @@
 from base_classes import *
+from queue import Queue
 
 '''
 This section and sequences are the crux of pyuvm. The classes here allow us to build classic UVM
@@ -38,11 +39,17 @@ We've opted for the latter.
         13.1.2.1---This is new() in the IEEE-UVM, but we mean
         the same thing with __init__()
         '''
+
+        component_dict={}
+
         super().__init__(name)
         self.__parent = None
         self.__children={}
         self.parent = parent
         self.print_enabled=True #13.1.2.2
+
+        # Cache the hierarchy for easy access
+        uvm_component.component_dict[self.full_name]=self
 
         @property
         def parent(self):
@@ -129,7 +136,244 @@ We've opted for the latter.
             assert(isinstance(name,str))
             return name in self.children
 
+        def lookup(self, name):
+            '''
+            13.1.3.7
+            Return a component base on the path. If . then
+            use full name from root otherwise relative
+            :param name: The search name
+            :return: either the component or None
+            '''
+            assert(name,str)
+            if name[0] == '.':
+                lookup_name=name
+            else:
+                lookup_name=f'{self.full_name}.{name}'
+            try:
+                return uvm_component.component_dict[lookup_name]
+            except KeyError:
+                return None
 
+
+        def get_depth(self):
+            '''
+            13.1.3.8
+            Get the depth that I am from the top component.
+            :param self: That's me
+            :return: depth
+            '''
+            # Rather than getting all recursive just count
+            # levels in the full name.
+            return len(self.full_name.split("."))-1
+
+        '''
+        Phasing Interface 13.1.4
+        We are only implementing the basic phases.  Fancy user-defined phasing could be added
+        at a later time.
+        
+        One note. The SystemVerilog UVM uses virtual methods for phases.
+        This is unnecessary in Python. Duck typing means that the variable has only
+        the type of the object in it.
+        
+        Therefore we do not implement the phases using the virtual method approach.
+        
+        '''
+
+        def build_phase(self):
+            '''
+            13.1.4.1.1
+            :param self:
+            :return: None
+            '''
+            pass
+
+        def connect_phase(self):
+            '''
+            13.1.4.1.2
+            :param self:
+            :return: None
+            '''
+            pass
+
+        def end_of_elaboration_phase(self):
+            '''
+            13.1.4.1.3
+            :param self:
+            :return: None
+            '''
+
+
+        def start_of_simulaiton_phase(self):
+            '''
+            13.1.4.1.4
+            :param self:
+            :return: None
+            '''
+            pass
+
+        def run_phase(self):
+            '''
+            13.1.4.1.5
+            :param self:
+            :return: None
+            '''
+            pass
+
+        def extract_phase(self):
+            '''
+            13.1.4.1.6
+            :param self:
+            :return: None
+            '''
+            pass
+
+        def check_phase(self):
+            '''
+            13.1.4.1.7
+            :param self:
+            :return: None
+            '''
+            pass
+
+        def report_phase(self):
+            '''
+            13.1.4.1.8
+            :param self:
+            :return: None
+            '''
+            pass
+
+        def final_phase(self):
+            '''
+            13.1.4.1.9
+            :param self:
+            :return: None
+            '''
+            pass
+
+        '''
+        The following sections have been skipped and could
+        be implemented at a later time if necessary.
+        13.1.4.2--Run time phases
+        13.1.4.3--phase_* methods
+        13.1.4.4--*_domain methods
+        13.1.5.5--suspend
+        13.1.4.6--pre_abort
+        13.1.5--Configurtion interface
+        13.1.6--Recording interface
+        13.1.7--Other interfaces
+        '''
+
+class uvm_test(uvm_component):
+    '''
+    13.2
+    The base class for all user-defined tests
+
+    Python does not require that we override new() for every
+    UVM class, so we don't do that (same for __init__).
+    '''
+
+
+class uvm_env(uvm_component):
+    '''
+    13.3
+    The user's containes for agents and what-not
+    '''
+
+
+class uvm_agent(uvm_component):
+    '''
+    13.4
+    Contains controls for individual agents
+    '''
+    def __init__(self):
+        super().__init__()
+        self.__is_active=False
+
+
+    '''
+    Have chosen to implement the spirit of 
+    the is_active member rather than the 
+    enum-based implementation.
+    '''
+    @property
+    def is_active(self):
+        return self.__is_active
+    @is_active.setter
+    def is_active(self, is_active):
+        assert(is_active,bool)
+        self.__is_active=is_active
+
+class uvm_monitor(uvm_component):
+    '''
+    13.5
+    We'll see if anything is ever added
+    to uvm_monitor
+    '''
+
+class uvm_scoreboard(uvm_component):
+    '''
+    13.6
+    '''
+
+class uvm_driver(uvm_component):
+    '''
+    13.7
+    
+    The pyuvm driver implementation differs from the SystemVerilog
+    UVM implementation is several respects.
+    
+    First, there is no need to paramterize the sequence items
+    going through the driver.  Python uses dynamic duck-typing,
+    so there is no need to ensure that all elements of the driver
+    correctly declare their variables.
+    
+    Second, Python provides the Queue class that does everything
+    that UVM TLM does.  So rather than reinvent, or wrap, the wheel
+    we will just use Python Queue objects to control our
+    sequence item flow.
+    
+    We will have a property named sequence_item_port but
+    it will be a Queue.
+    
+    Our sequence_item_port will NOT return a response back. Generally
+    we want to encourge using the object handles as the method of 
+    transfer.  But there is a rsp_port for those who insist.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.__sequence_item_port=None
+        self.__rsp_port=None
+
+    @property
+    def sequence_item_port(self):
+        return self.__sequence_item_port
+
+    @sequence_item_port.setter
+    def sequence_item_port(self,port):
+        assert(isinstance(port, Queue))
+        self.__sequence_item_port=port
+
+    @property
+    def rsp_port(self):
+        return self.__rsp_port
+
+    @rsp_port.setter
+    def rsp_port(self, port):
+        assert(isinstance(port,Queue))
+        self.__rsp_port=port
+
+'''
+13.8 uvm_push_driver
+
+Never seen one used. Not implemented.
+'''
+
+class uvm_subscriber(uvm_component):
+    '''
+    13.9
+    '''
 
 
 
