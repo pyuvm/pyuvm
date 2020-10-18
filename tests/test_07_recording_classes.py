9eaa3ecacb41a58243413541c119becee3019ac1
raysalemi <ray@raysalemi.com>
2020-10-18 17:26:48 -0400

    Now have no tests in section 7 but have that intentionally

--- tests/test_07_recording_classes.py
+++ tests/test_07_recording_classes.py
@@ -3,6 +3,9 @@ from pyuvm import *
 
 class s07_recording_classes_TestCase (pyuvm_unittest.pyuvm_TestCase):
     """
-    There is no functionality and so no tests.
+    There is no functionality and so no tests. But I want to show
+    that these tests have been intentionally omitted.
     """
-    pass
+    def test_no_functionality(self):
+        pass
+
