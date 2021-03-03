af3410f80a33512bfa15f58677515550a9517ac8
raysalemi <ray@raysalemi.com>
2021-03-03 08:29:32 -0500

    Fixed description in pip

--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
-    readme = f.read()
+
+readme = """
+# Description
+
+**pyuvm** implements the most often-used parts of the UVM while taking advantage of the fact that Python does not have strict typing and does not require parameterized classes. The project refactors pieces of the UVM that were either overly complicated due to typing or legacy code.
+
+The code is based in the IEEE 1800.2 specification and most classes and methods have the specification references in the comments.
+"""
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
     name='pyuvm',
-    version='1.0',
+    version='1.0.1',
     description='Python Implementation of Universal Verification Methodology',
     long_description=readme,
     long_description_content_type="text/markdown",
