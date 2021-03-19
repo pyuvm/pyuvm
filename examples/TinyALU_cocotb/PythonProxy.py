c91522399f5049241c3954cc6a4f06d8ce7a0071
Ray Salemi <ray_salemi@mentor.com>
2021-03-19 10:17:14 -0700

    Now using the final example from Python for UVM Verification

--- examples/TinyALU_cocotb/PythonProxy.py
+++ examples/TinyALU_cocotb/PythonProxy.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import enum
 from pyuvm import *
 import random
@@ -10,44 +16,8 @@ class Ops(enum.IntEnum):
     XOR = 3
     MUL = 4
 
-class AluCommand(uvm_transaction):
-
-    def __init__(self, name, A=0, B=0, op=Ops.ADD):
-        super().__init__(name)
-        self.A = A
-        self.B = B
-        self.op = Ops(op)
-
-    def __eq__(self, other):
-        same = self.A == other.A \
-               and self.B == other.B \
-               and self.op == other.op
-        return same
-
-    def __str__(self):
-        return f"{self.get_name()} : A: 0x{self.A:02x} OP: {self.op.name} ({self.op.value}) B: 0x{self.B:02x}"
-    
-    def randomize(self):
-        self.A = random.randint(0,255)
-        self.B = random.randint(0,255)
-        self.op = random.choice(list(Ops))
-
-class AluResult(uvm_transaction):
-    def __init__(self, name, r):
-        super().__init__(name)
-        self.result = r
-
-    def __str__(self):
-        return f"{self.get_name()}: 0x{self.result:04x}"
-
-    def __eq__(self, other):
-        return self.result == other.result
-
 
 class PythonProxy(uvm_component):
-    def __init__(self, name, parent, label):
-        super().__init__(name, parent)
-        ConfigDB().set(None, "*", label, self)
     @staticmethod
     def alu_op(A, B, op):
         result = None # Make the linter happy
@@ -92,8 +62,8 @@ class PythonProxy(uvm_component):
         self.result_put.connect(self.result_f.put_export)
         self.result_get.connect(self.result_f.get_export)
 
-    def send_op(self, aa, bb, op):
-        self.stim_put.put((aa, bb, op))
+    def send_op(self, A, B, op):
+        self.stim_put.put((A, B, op))
     
     def get_cmd(self):
         cmd  = self.cmd_get.get()
@@ -104,9 +74,10 @@ class PythonProxy(uvm_component):
         return result        
         
     def run_phase(self):
-        while True:
-            (aa, bb, op) = self.stim_get.get()
-            result = self.alu_op(aa, bb, op)
-            self.cmd_put.put((aa, bb, op))
+        while not ObjectionHandler().run_phase_complete():
+            (A, B, op) = self.stim_get.get()
+            result = self.alu_op(A, B, op)
+            self.cmd_put.put((A, B, op))
             self.result_put.put(result)
             
+
