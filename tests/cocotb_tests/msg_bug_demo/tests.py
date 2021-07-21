606dd0b17b89746da5e1e7bb520b6f34101b330b
raysalemi <ray@raysalemi.com>
2021-07-21 07:41:47 -0400

    Now with code demonstrating the bug

---
+++ tests/cocotb_tests/msg_bug_demo/tests.py
@@ -0,0 +1,19 @@
+from pyuvm import *
+import pyuvm.utility_classes as utility_classes
+
+
+class TestHolder:
+
+    def setUp(self):
+        self.top = uvm_component("top", None)
+
+    def tearDown(self):
+        uvm_root.clear_singletons()
+        pass
+
+    def test_no_assert(self):
+        pass
+
+    def test_no_assert2(self):
+        pass
+
