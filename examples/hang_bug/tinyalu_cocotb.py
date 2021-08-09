c1faa85f67b84dde3592bc6c8ef12b92aba0c814
raysalemi <ray@raysalemi.com>
2021-08-09 18:23:47 -0400

    TinyALU hangs example does not.

--- examples/hang_bug/tinyalu_cocotb.py
+++ examples/hang_bug/tinyalu_cocotb.py
@@ -1,12 +1,12 @@
-from cocotb.clock import Clock
 from cocotb.triggers import FallingEdge
-from cocotb.queue import QueueEmpty
 import cocotb
+from cocotb.queue import Queue
 
 class ProdCon():
     def __init__(self, dut) -> None:
         self.dut = dut
-        self.qq = cocotb.queue.Queue(maxsize = 1)
+        self.qq = Queue(maxsize = 1)
+        self.ev = cocotb.triggers.Event()
 
     async def producer(self, rangenum = 5):
         for ii in range(rangenum):
@@ -20,10 +20,16 @@ class ProdCon():
             print("saw falling edge")
             datum = await self.qq.get()
             print(datum)
+            if datum == 4:
+                self.ev.set()
+
+    async def go(self):
+        cocotb.fork(self.producer())
+        cocotb.fork(self.consumer())
+        await self.ev.wait()
 
 @cocotb.test()
 async def stuck(dut):
     """ test clock hang"""
     pc = ProdCon(dut)
-    await pc.producer()
-    await pc.consumer()
+    await pc.go()
