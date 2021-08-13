978bdfc6dcbd6f2468b8c15da4a67a827e6af116
raysalemi <ray@raysalemi.com>
2021-08-13 17:29:31 -0400

    now python setup without package path

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyuvm",
+    version="2.0a7",
+    author="Ray Salemi",
+    author_email="ray@raysalemi.com",
+    description="A Python implementation of the UVM using cocotb",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pyuvm/pyuvm",
+    project_urls={
+        "Bug Tracker": "https://github.com/pyuvm/pyuvm/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+    ],
+    packages=setuptools.find_packages(where="pyuvm"),
+    python_requires=">=3.6",
+    install_requires="cocotb>=1.5.2",
+)
