ea22cc1453d1f26418bbb5347d50313dc23605f4
raysalemi <ray@raysalemi.com>
2021-08-13 17:56:34 -0400

    finally installed at 2.0a8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyuvm",
-    version="2.0a7",
+    version="2.0a8",
     author="Ray Salemi",
     author_email="ray@raysalemi.com",
     description="A Python implementation of the UVM using cocotb",
@@ -21,7 +21,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
     ],
-    packages=setuptools.find_packages(where="pyuvm"),
+    packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires="cocotb>=1.5.2",
 )
