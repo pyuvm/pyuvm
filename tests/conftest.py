90539ac1a592169b5753679e5339d4477cbd7a98
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-21 19:14:25 +0200

    Extract fixture that re-initializes 'pyuvm'
    This is a preparatory refactoring before moving the micro-tests outside of
    'TestCase' classes.

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from pyuvm.s13_predefined_component_classes import uvm_component, uvm_root
+
+@pytest.fixture()
+def initialize_pyuvm(request):
+    uvm_component.component_dict.clear()
+    uvm_root.component_dict.clear()
+    uvm_root.clear_singletons()
+    pass
