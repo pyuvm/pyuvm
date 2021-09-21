e02633c86bcd2e53988632fbb4e72bd92a5e2d3e
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-21 19:58:04 +0200

    Extract focused test for logging of messages
    It's going to make it much easier to move to 'pytest', because it will make
    the steps more obvious.

--- tests/pytests/test_06_reporting_classes.py
+++ tests/pytests/test_06_reporting_classes.py
@@ -12,12 +12,27 @@ class s06_reporting_classes_TestCase(pyuvm_unittest.pyuvm_TestCase):
         ro = uvm_report_object('ro')
         assert hasattr(ro, "logger")
         assert not ro.logger.propagate
+
+    def test_logging_of_debug_messages(self):
+        ro = uvm_report_object('ro')
         with self.assertLogs(ro.logger, level='DEBUG') as cm:
             ro.logger.debug('debug')
+            assert cm.output == [f'DEBUG:uvm.ro{id(ro)}:debug']
+
+    def test_logging_of_info_messages(self):
+        ro = uvm_report_object('ro')
+        with self.assertLogs(ro.logger, level='DEBUG') as cm:
             ro.logger.info('info')
+            assert cm.output == [f'INFO:uvm.ro{id(ro)}:info']
+
+    def test_logging_of_error_messages(self):
+        ro = uvm_report_object('ro')
+        with self.assertLogs(ro.logger, level='DEBUG') as cm:
             ro.logger.error('error')
+            assert cm.output == [f'ERROR:uvm.ro{id(ro)}:error']
+
+    def test_logging_of_critical_messages(self):
+        ro = uvm_report_object('ro')
+        with self.assertLogs(ro.logger, level='DEBUG') as cm:
             ro.logger.critical('critical')
-            assert cm.output == [f'DEBUG:uvm.ro{id(ro)}:debug',
-                                 f'INFO:uvm.ro{id(ro)}:info',
-                                 f'ERROR:uvm.ro{id(ro)}:error',
-                                 f'CRITICAL:uvm.ro{id(ro)}:critical']
+            assert cm.output == [f'CRITICAL:uvm.ro{id(ro)}:critical']
