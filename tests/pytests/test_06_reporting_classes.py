e2b2b808c9e844195ac7ceceae01391af5218c43
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-22 10:21:20 +0200

    Remove reliance on 'unittest.TestCase' from tests for reporting

--- tests/pytests/test_06_reporting_classes.py
+++ tests/pytests/test_06_reporting_classes.py
@@ -3,45 +3,38 @@ import pyuvm_unittest
 from pyuvm import *
 
 
-class s06_reporting_classes_TestCase(pyuvm_unittest.pyuvm_TestCase):
-    """Basic test cases."""
+def test_object_creation():
+    """
+    Test that we actually get a logger in our object.
+    """
+    ro = uvm_report_object('ro')
+    assert hasattr(ro, "logger")
+    assert not ro.logger.propagate
 
-    @pytest.fixture(autouse=True)
-    def inject_fixtures(self, caplog):
-        self._caplog = caplog
+def test_logging_of_debug_messages(caplog):
+    ro = uvm_report_object('ro')
+    ro.logger.propagate = True # workaround for 'caplog'
+    caplog.set_level(logging.DEBUG, ro.logger.name)
+    ro.logger.debug('debug')
+    assert caplog.record_tuples == [(ro.logger.name, logging.DEBUG, 'debug')]
 
-    def test_object_creation(self):
-        """
-        Test that we actually get a logger in our object.
-        """
-        ro = uvm_report_object('ro')
-        assert hasattr(ro, "logger")
-        assert not ro.logger.propagate
+def test_logging_of_info_messages(caplog):
+    ro = uvm_report_object('ro')
+    ro.logger.propagate = True # workaround for 'caplog'
+    caplog.set_level(logging.DEBUG)
+    ro.logger.info('info')
+    assert caplog.record_tuples == [(ro.logger.name, logging.INFO, 'info')]
 
-    def test_logging_of_debug_messages(self):
-        ro = uvm_report_object('ro')
-        ro.logger.propagate = True # workaround for 'caplog'
-        self._caplog.set_level(logging.DEBUG, ro.logger.name)
-        ro.logger.debug('debug')
-        assert self._caplog.record_tuples == [(ro.logger.name, logging.DEBUG, 'debug')]
+def test_logging_of_error_messages(caplog):
+    ro = uvm_report_object('ro')
+    ro.logger.propagate = True # workaround for 'caplog'
+    caplog.set_level(logging.DEBUG)
+    ro.logger.error('error')
+    assert caplog.record_tuples == [(ro.logger.name, logging.ERROR, 'error')]
 
-    def test_logging_of_info_messages(self):
-        ro = uvm_report_object('ro')
-        ro.logger.propagate = True # workaround for 'caplog'
-        self._caplog.set_level(logging.DEBUG)
-        ro.logger.info('info')
-        assert self._caplog.record_tuples == [(ro.logger.name, logging.INFO, 'info')]
-
-    def test_logging_of_error_messages(self):
-        ro = uvm_report_object('ro')
-        ro.logger.propagate = True # workaround for 'caplog'
-        self._caplog.set_level(logging.DEBUG)
-        ro.logger.error('error')
-        assert self._caplog.record_tuples == [(ro.logger.name, logging.ERROR, 'error')]
-
-    def test_logging_of_critical_messages(self):
-        ro = uvm_report_object('ro')
-        ro.logger.propagate = True # workaround for 'caplog'
-        self._caplog.set_level(logging.DEBUG)
-        ro.logger.critical('critical')
-        assert self._caplog.record_tuples == [(ro.logger.name, logging.CRITICAL, 'critical')]
+def test_logging_of_critical_messages(caplog):
+    ro = uvm_report_object('ro')
+    ro.logger.propagate = True # workaround for 'caplog'
+    caplog.set_level(logging.DEBUG)
+    ro.logger.critical('critical')
+    assert caplog.record_tuples == [(ro.logger.name, logging.CRITICAL, 'critical')]
