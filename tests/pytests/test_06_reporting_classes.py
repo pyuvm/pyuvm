f304801bf2acf5c54f61e52c7b837eb52b509612
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-22 10:42:10 +0200

    Generate tests for logging using test parameterization

--- tests/pytests/test_06_reporting_classes.py
+++ tests/pytests/test_06_reporting_classes.py
@@ -11,30 +11,13 @@ def test_object_creation():
     assert hasattr(ro, "logger")
     assert not ro.logger.propagate
 
-def test_logging_of_debug_messages(caplog):
+@pytest.mark.parametrize('level,msg', [(logging.DEBUG, 'debug'),
+                                       (logging.INFO, 'info'),
+                                       (logging.ERROR, 'error'),
+                                       (logging.CRITICAL, 'critical')])
+def test_logging(level, msg, caplog):
     ro = uvm_report_object('ro')
     ro.logger.propagate = True # workaround for 'caplog'
     caplog.set_level(logging.DEBUG, ro.logger.name)
-    ro.logger.debug('debug')
-    assert caplog.record_tuples == [(ro.logger.name, logging.DEBUG, 'debug')]
-
-def test_logging_of_info_messages(caplog):
-    ro = uvm_report_object('ro')
-    ro.logger.propagate = True # workaround for 'caplog'
-    caplog.set_level(logging.DEBUG)
-    ro.logger.info('info')
-    assert caplog.record_tuples == [(ro.logger.name, logging.INFO, 'info')]
-
-def test_logging_of_error_messages(caplog):
-    ro = uvm_report_object('ro')
-    ro.logger.propagate = True # workaround for 'caplog'
-    caplog.set_level(logging.DEBUG)
-    ro.logger.error('error')
-    assert caplog.record_tuples == [(ro.logger.name, logging.ERROR, 'error')]
-
-def test_logging_of_critical_messages(caplog):
-    ro = uvm_report_object('ro')
-    ro.logger.propagate = True # workaround for 'caplog'
-    caplog.set_level(logging.DEBUG)
-    ro.logger.critical('critical')
-    assert caplog.record_tuples == [(ro.logger.name, logging.CRITICAL, 'critical')]
+    ro.logger.log(level, msg)
+    assert caplog.record_tuples == [(ro.logger.name, level, msg)]
