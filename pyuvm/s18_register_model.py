7d96a64de996a91426bb5aee466699006e09d34f
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-22 12:57:22 +0200

    Add empty `uvm_reg_map` class

--- pyuvm/s18_register_model.py
+++ pyuvm/s18_register_model.py
@@ -4,3 +4,8 @@ from pyuvm import uvm_object
 # 18.1.1 Class declaration
 class uvm_reg_block(uvm_object):
     pass
+
+
+# 18.2.1 Class declaration
+class uvm_reg_map(uvm_object):
+    pass
--- tests/pytests/test_18_register_model.py
+++ tests/pytests/test_18_register_model.py
@@ -3,3 +3,7 @@ from pyuvm import *
 
 def test_reg_block():
     assert uvm_reg_block()
+
+
+def test_reg_map():
+    assert uvm_reg_map()
