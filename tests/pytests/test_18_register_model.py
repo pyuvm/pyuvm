61263a402de273b029f6939094db478d3fdc4c9f
Tudor Timi <tudortimi@users.noreply.github.com>
2021-09-23 19:54:17 +0200

    Implement configuration of `uvm_reg_field` reset

--- tests/pytests/test_18_register_model.py
+++ tests/pytests/test_18_register_model.py
@@ -16,17 +16,18 @@ def test_reg():
 def test_reg_field_configure():
     field = uvm_reg_field()
     parent = uvm_reg()
-    field.configure(parent, 8, 16, 'RW', True)
+    field.configure(parent, 8, 16, 'RW', True, 15)
     assert field.get_parent() == parent
     assert field.get_n_bits() == 8
     assert field.get_lsb_pos() == 16
     assert field.get_access() == 'RW'
     assert field.is_volatile()
+    assert field.get_reset() == 15
 
 
 def test_reg_field_is_volatile():
     field = uvm_reg_field()
-    field.configure(uvm_reg(), 8, 16, 'RW', True)
+    field.configure(uvm_reg(), 8, 16, 'RW', True, 15)
     assert field.is_volatile()
-    field.configure(uvm_reg(), 8, 16, 'RW', False)
+    field.configure(uvm_reg(), 8, 16, 'RW', False, 15)
     assert not field.is_volatile()
