388202b144782ae2ec12b80c7a6bc21473b00328
Ray Salemi <ray@raysalemi.com>
2024-04-16 15:23:05 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyuvm",
-    version="2.9.1",
+    version="3.0.0.dev0",
     author="Ray Salemi",
     author_email="ray@raysalemi.com",
     description="A Python implementation of the UVM using cocotb",
