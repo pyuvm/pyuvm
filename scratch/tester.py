0c86474741022d0e9444070837fcc55745076178
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-10-07 21:16:04 -0600

    Don't check in scratch dir

--- scratch/tester.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-
-class Tester:
-    @staticmethod
-    def run_test(test):
-        tt = threading.Thread(target=test, name="test_thread")
-        tt.start()
-        tt.join()
