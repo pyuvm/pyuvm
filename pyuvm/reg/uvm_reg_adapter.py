7d2cf5612e4a1738ec5def754a096c7b8b969c1d
Rasmus Olsen <22944947+RasmusGOlsen@users.noreply.github.com>
2025-10-16 13:12:00 +0200

    uvm_reg_adapter implementation

--- pyuvm/reg/uvm_reg_adapter.py
+++ pyuvm/reg/uvm_reg_adapter.py
@@ -1,30 +1,42 @@
-from pyuvm import uvm_object, uvm_sequence_item
-from pyuvm.reg.uvm_reg_item import uvm_reg_bus_op
+from typing import TYPE_CHECKING
+
+from pyuvm.reg.uvm_reg_item import uvm_reg_bus_op, uvm_reg_item
+from pyuvm.s05_base_classes import uvm_object
+from pyuvm.s14_15_python_sequences import uvm_sequence_item
+
+if TYPE_CHECKING:
+    from pyuvm.s14_15_python_sequences import uvm_sequence_base
 
 __all__ = ["uvm_reg_adapter", "uvm_reg_tlm_adapter"]
 
 
 class uvm_reg_adapter(uvm_object):
     def __init__(self, name: str = ""):
-        raise NotImplementedError
+        self.supports_byte_enable: bool = False
+        self.provides_response: bool = False
+        self.parent_sequence: uvm_sequence_base = None
+        self._item: uvm_reg_item = None
+
+    def get_item(self) -> uvm_reg_item:
+        return self._item
+
+    def set_item(self, item: uvm_reg_item) -> None:
+        self._item = item
 
     def reg2bus(self, rw: uvm_reg_bus_op) -> uvm_sequence_item:
-        raise NotImplementedError
+        pass
 
-    def bus2reg(self,
-                bus_item: uvm_sequence_item,
-                rw: uvm_reg_bus_op) -> None:
-        raise NotImplementedError
+    def bus2reg(self, bus_item: uvm_sequence_item, rw: uvm_reg_bus_op) -> None:
+        pass
 
 
 class uvm_reg_tlm_adapter(uvm_reg_adapter):
-    def __init__(self, name: str = ""):
+    def __init__(self, name: str = "uvm_reg_tlm_adapter"):
         super().__init__(name)
+        raise NotImplementedError
 
     def reg2bus(self, rw: uvm_reg_bus_op) -> uvm_sequence_item:
         raise NotImplementedError
 
-    def bus2reg(self,
-                bus_item: uvm_sequence_item,
-                rw: uvm_reg_bus_op) -> None:
+    def bus2reg(self, bus_item: uvm_sequence_item, rw: uvm_reg_bus_op) -> None:
         raise NotImplementedError
