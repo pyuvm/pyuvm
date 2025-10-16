0beefadb6cbec83b62b66dce7d4161149cd24799
Rasmus Olsen <22944947+RasmusGOlsen@users.noreply.github.com>
2025-10-16 13:12:33 +0200

    uvm_reg_predictor implementation

--- pyuvm/reg/uvm_reg_predictor.py
+++ pyuvm/reg/uvm_reg_predictor.py
@@ -1,9 +1,19 @@
 from __future__ import annotations
 
-from pyuvm import uvm_component, uvm_phase
-from pyuvm.reg.uvm_reg_item import uvm_reg_item
+import logging
+from typing import TYPE_CHECKING
+
+from pyuvm.s12_uvm_tlm_interfaces import uvm_analysis_imp
+from pyuvm.s13_uvm_component import uvm_component
+
+if TYPE_CHECKING:
+    from pyuvm import uvm_analysis_port, uvm_phase
+    from pyuvm.reg.uvm_reg_adapter import uvm_reg_adapter
+    from pyuvm.reg.uvm_reg_item import uvm_reg_item
+    from pyuvm.reg.uvm_reg_map import uvm_reg_map
 
 __all__ = ["uvm_reg_predictor"]
+logger = logging.getLogger("RegModel")
 
 
 class uvm_predict_s:
@@ -13,14 +23,28 @@ class uvm_predict_s:
 
 class uvm_reg_predictor(uvm_component):
     def __init__(self, name: str, parent: uvm_component) -> None:
+        self.bus_in: uvm_analysis_imp = uvm_analysis_imp("bus_in")
+        self.reg_ap: uvm_analysis_port = uvm_analysis_port("reg_ap")
+        self.map: uvm_reg_map = None
+        self.adapter: uvm_reg_adapter = None
+        self._pending: list[uvm_predict_s] = list()
         raise NotImplementedError
 
     def pre_predict(self, rw: uvm_reg_item) -> None:
-        raise NotImplementedError
+        raise NotImplementedError("Must be implemented by user")
 
     def check_phase(self, phase: uvm_phase):
-        super().check_phase()
-        raise NotImplementedError
+        super().check_phase(phase)
+        if self._pending:
+            string: str = ""
+            for reg in self._pending:
+                string += f"{reg.get_full_name()}\n"
+
+            logger.error(
+                f"There are {len(self._pending)} incomplete "
+                "register transactions still pending completion:\n"
+                f"{string}"
+            )
 
     def flush(self) -> None:
-        raise NotImplementedError
+        self._pending.clear()
