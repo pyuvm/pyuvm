b641230c3453aa52eb751fc1a71fa66416e52c2a
Rasmus Olsen <22944947+RasmusGOlsen@users.noreply.github.com>
2025-10-19 00:34:37 +0200

    Replace UVM RAL implementation

--- pyuvm/__init__.py
+++ pyuvm/__init__.py
@@ -6,6 +6,26 @@ from pyuvm.error_classes import *
 # Extension Modules
 from pyuvm.extension_classes import *
 
+# Secttion 18 Register Layer
+from pyuvm.reg.uvm_mem import *
+from pyuvm.reg.uvm_mem_mam import *
+from pyuvm.reg.uvm_reg import *
+from pyuvm.reg.uvm_reg_adapter import *
+from pyuvm.reg.uvm_reg_backdoor import *
+from pyuvm.reg.uvm_reg_block import *
+from pyuvm.reg.uvm_reg_cbs import *
+from pyuvm.reg.uvm_reg_field import *
+from pyuvm.reg.uvm_reg_fifo import *
+from pyuvm.reg.uvm_reg_file import *
+from pyuvm.reg.uvm_reg_indirect import *
+from pyuvm.reg.uvm_reg_item import *
+from pyuvm.reg.uvm_reg_map import *
+from pyuvm.reg.uvm_reg_model import *
+from pyuvm.reg.uvm_reg_predictor import *
+from pyuvm.reg.uvm_reg_sequence import *
+from pyuvm.reg.uvm_vreg import *
+from pyuvm.reg.uvm_vreg_field import *
+
 # Section 5
 from pyuvm.s05_base_classes import *
 
@@ -29,21 +49,4 @@ from pyuvm.s13_predefined_component_classes import *
 
 # Section 14, 15 (Done as fresh Python design)
 from pyuvm.s14_15_python_sequences import *
-
-# Section 17, Register enumeration
-from pyuvm.s17_uvm_reg_enumerations import *
-
-# from pyuvm.s17_register_enumerations import *
-#
-#  Section 18
-# from pyuvm.s18_register_model import *
-from pyuvm.s18_uvm_reg_block import uvm_reg_block
-from pyuvm.s19_uvm_reg_field import uvm_reg_field
-from pyuvm.s20_uvm_reg import uvm_reg
-from pyuvm.s21_uvm_reg_map import uvm_reg_map
-from pyuvm.s22_uvm_mem import uvm_mem
-from pyuvm.s23_uvm_reg_item import uvm_reg_item
-from pyuvm.s24_uvm_reg_includes import *
-from pyuvm.s25_uvm_adapter import uvm_reg_adapter
-from pyuvm.s26_uvm_predictor import uvm_reg_predictor
 from pyuvm.utility_classes import *
