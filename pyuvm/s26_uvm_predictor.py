dd8372bc4114b179f3bf8950ee67f92faa4ce154
Rasmus Olsen <22944947+RasmusGOlsen@users.noreply.github.com>
2025-10-31 12:48:54 +0100

    Cleanup old ral register model implementation

--- pyuvm/s26_uvm_predictor.py
+++
@@ -1,9 +0,0 @@
-# Import Main Package
-from pyuvm.s05_base_classes import uvm_object
-
-
-# Main Class
-class uvm_reg_predictor(uvm_object):
-    # Constructor
-    def __init__(self, name="uvm_reg_predictor"):
-        super().__init__(name)
