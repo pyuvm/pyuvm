ee4c07516cadcdacb9e26decfe6b855d53de462a
Stephan Epp <160559413+hjstephan86@users.noreply.github.com>
2025-11-28 19:45:05 +0100

    Testbench for aoi_2_2.v and aoi_2_2.vhd with Line-by-Line Coverage (#306)

---
+++ examples/aoi_2_2/checkclean.mk
@@ -0,0 +1,15 @@
+CURRDIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+check:
+	$(CURRDIR)/combine_results.py
+
+testclean: clean
+	@rm -rf __pycache__
+	@rm -rf results.xml
+	@rm -rf combined_results.xml
+	@rm -rf log.txt
+	@rm -rf sim_build
+	@rm -rf modelsim.ini
+	@rm -rf transcript
+
+checkclean: check testclean
